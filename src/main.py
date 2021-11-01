dea10ec41579964221f4e8fc0de1789c35cd16af
vijay <vijaybalajitheboss@gmail.com>
2021-11-01 22:27:47 +0530

    Completed setting up connections

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
+from modules.connection import connection
 import psycopg2
 import os
 
 if __name__ == "__main__":
     load_dotenv()
     conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-    print("Connected successfully")
+    connection(conn)
