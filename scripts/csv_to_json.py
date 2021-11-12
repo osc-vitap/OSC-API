eb732ae762d66b95e291f89da40eaaba6c03232d
vijay <vijaybalajitheboss@gmail.com>
2021-11-12 11:59:46 +0530

    Added sample script that converts csv data to json

---
+++ scripts/csv_to_json.py
@@ -0,0 +1,67 @@
+import csv
+import json
+
+"""
+Convert CSV to JSON. 
+Use this script after collecting EB member details from Google spreadsheet.
+Download the spreadsheet and add it's path to line 29.
+Once completed, you should get your JSON file
+"""
+
+
+def csv_to_json(csvFile, jsonFile):
+    json_data = {}
+    heirarchy_order = {
+        "Admin Department": [
+            "Club Coordinator",
+            "Community Leader",
+            "President",
+            "Director",
+            "Chairman",
+            "Treasurer",
+            "Vice President",
+            "Secretary",
+            "Head of public relations",
+            "Track lead",
+            "HR Lead",
+        ],
+        "Event Department": [
+            "Event lead",
+            "Social Media Lead",
+            "Organizer",
+        ],
+        "Tech Department": [
+            "Technical Lead",
+            "Developer",
+        ],
+        "Design Team": [
+            "Head of marketing",
+            "Chief Editor",
+            "Head of videography",
+            "Documentation Specialist",
+            "Designer",
+        ],
+    }
+    All_positions = []
+    for key, value in heirarchy_order.items():
+        for i in value:
+            All_positions.append(i)
+
+    with open(csvFile, encoding="utf-8") as f:
+        csvReader = list(csv.DictReader(f))
+        for position in All_positions:
+            for rows in csvReader:
+                key = rows["Position"]
+                print(position, rows["Position"])
+                if position in rows["Position"]:
+                    if key in json_data:
+                        json_data[key].append(rows)
+                    else:
+                        json_data[key] = [rows]
+
+    with open(jsonFile, "w", encoding="utf-8") as f:
+        f.write(json.dumps(json_data, indent=4, separators=(",", ": ")))
+
+
+if __name__ == "__main__":
+    csv_to_json(csvFile="examples/ebDetails.csv", jsonFile="examples/ebDetails.json")
