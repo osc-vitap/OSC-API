0825234242437cee6c0a36d047476ba599df674e
vijay <vijaybalajitheboss@gmail.com>
2021-12-12 13:54:52 +0530

    Updated endpoints to accept eventID as an argument.

--- src/routes/events/event.py
+++ src/routes/events/event.py
@@ -8,17 +8,19 @@ event_bp = Blueprint("events", __name__, url_prefix="/event")
 
 @event_bp.route("/", methods=["GET"])
 def get_data():
-    data = connection()
-    return jsonify(data)
-
-
-@event_bp.route("/<int:id>", methods=["GET"])
-def get_id(id):
-    data = connection("eventID", id)
-    if data:
-        return jsonify(data)
+    event_id = request.args.get("id")
+    if not event_id:
+        data = connection()
     else:
-        return "ERROR 404: CANNOT GET {}".format(request.path)
+        data = connection("eventID", event_id)
+        if data:
+            return jsonify(data)
+        else:
+            return (
+                "ERROR 404: Event with ID {} not found".format(event_id),
+                404,
+            )
+    return jsonify(data)
 
 
 @event_bp.route("/latest", methods=["GET"])
@@ -48,6 +50,11 @@ def make_announcement():
         required_event = connection("latest")
     else:
         required_event = connection("eventID", event_id)
-
+        if not required_event:
+            return (
+                "ERROR 404: Event with ID {} not found".format(event_id),
+                404,
+            )
+        print(required_event)
     status = send_discord_announcement(WEBHOOK_URL, required_event)
     return jsonify({"success": status})
